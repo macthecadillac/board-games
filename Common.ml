09926f076c2c6f09976897e286b6fdbb93052711
Mac Lee <macthecadillac@gmail.com>
2019-02-22 17:04:49 -0800

    Moved abstype into common

--- Common.ml
+++ Common.ml
@@ -1,6 +1,60 @@
 open Containers
-module Count = AbsType.MakeMInt (AbsType.I)
-module Index = AbsType.MakeMInt (AbsType.I)
+
+module type I = sig
+  type t = int
+  val to_int : t -> int
+  val of_int : int -> t
+end
+
+module type S = functor (M : I) -> sig
+  type t
+  val init : unit -> t
+  val to_int : t -> int
+  val of_int : int -> t
+  val inc : t -> t
+  val dec : t -> t
+  val ( + ) : t -> t -> t
+  val ( - ) : t -> t -> t
+  val ( > ) : t -> t -> bool
+  val ( < ) : t -> t -> bool
+  val ( = ) : t -> t -> bool
+end
+
+module MakeI : S =
+  functor (M : I) -> struct
+  type t = M.t
+
+  let init () = M.of_int 0
+
+  let of_int = M.of_int
+
+  let to_int = M.to_int
+
+  let inc n = to_int n |> ( + ) 1 |> of_int
+
+  let dec n = to_int n - 1 |> of_int
+
+  let ( + ) a b = to_int a + to_int b |> of_int
+
+  let ( - ) a b = to_int a - to_int b |> of_int
+
+  let ( > ) a b = to_int a > to_int b
+
+  let ( < ) a b = to_int a < to_int b
+
+  let ( = ) a b = to_int a = to_int b
+end
+
+module I = struct
+  type t = int
+
+  let of_int n = n
+
+  let to_int n = n
+end
+
+module Count = MakeI (I)
+module Index = MakeI (I)
 
 type player = One | Two
 
