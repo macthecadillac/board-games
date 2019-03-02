af0c787bedf3c104e81d015a45d665985200d20d
Mac Lee <macthecadillac@gmail.com>
2019-03-02 15:19:55 -0800

    Removed unused functor type declarations

--- MCTS.mli
+++ MCTS.mli
@@ -2,14 +2,11 @@ open Common
 
 module type BOARD = sig
   type t
-  val init : unit -> t
   val available_moves : t -> Index.t list
   val curr_player : t -> player
   val is_finished : t -> bool
-  val is_valid_move : Index.t -> t -> bool
   val move : Index.t -> t -> t
   val winner_is : t -> player option
-  val print : t -> unit
 end
 
 module type S = sig
