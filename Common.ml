4d1e5277f8fec922822cda70b5fde6ed89cae57c
Mac Lee <macthecadillac@gmail.com>
2019-03-04 00:28:22 -0800

    Expanded the BOARD type to include more necessary functions

--- Common.ml
+++ Common.ml
@@ -57,9 +57,12 @@ type debug = Debug | Release
 
 module type GAME = sig
   type t
+  val init : unit -> t
   val available_moves : t -> Index.t list
   val curr_player : t -> player
   val is_finished : t -> bool
+  val is_valid_move : Index.t -> t -> bool
   val move : Index.t -> t -> t
   val winner_is : t -> player option
+  val print : t -> unit
 end
--- Common.mli
+++ Common.mli
@@ -34,9 +34,12 @@ module Count : CustomInt.S
 
 module type GAME = sig
   type t
+  val init : unit -> t
   val available_moves : t -> Index.t list
   val curr_player : t -> player
   val is_finished : t -> bool
+  val is_valid_move : Index.t -> t -> bool
   val move : Index.t -> t -> t
   val winner_is : t -> player option
+  val print : t -> unit
 end
